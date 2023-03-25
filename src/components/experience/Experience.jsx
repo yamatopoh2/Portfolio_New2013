0382c059f46504e87bc8b26de4accfe98d3052d1
yamatopoh2 <reymartdellava30@gmail.com>
2023-03-25 12:16:15 +0800

    Updated Portfolio

---
+++ src/components/experience/Experience.jsx
@@ -0,0 +1,198 @@
+import React from "react"
+import "./experience.css"
+import { BsFillPatchCheckFill } from "react-icons/bs"
+const Experience = () => {
+  return (
+    <section id="experience">
+      <h2>My Skills</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>JavaScript</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Bootstrap/Tailwind</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>React</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>WordPress</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Rise360/Storyline</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* END OF FRONT END */}
+        {/* Start Back end */}
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Node JS</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>MySQL</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Microsoft Access</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Php</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Laravel</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* End Back end */}
+
+        {/* Start Server */}
+        <div className="experience__ServerDomains">
+          <h3>Server / Domain</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Aapanel Server</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>JumpServer (Linux)</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>AWS ec2</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>NameCheap</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Cloudflare</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* End Back end */}
+        {/* Start SEO */}
+        <div className="experience__Seo">
+          <h3>SEO / Optimization / Technical</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Optimization of Websites</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>ahrefs (Technical)</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Semrush (Technical)</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Sitemaps</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* End SEO */}
+      </div>
+    </section>
+  )
+}
+
+export default Experience
---
+++ src/components/workExperience/WorkExperience.jsx
@@ -0,0 +1,80 @@
+import React from "react"
+import "./workExperience.css"
+
+const WorkExperience = () => {
+  return (
+    <section id="workexperience">
+      <h2>Work Experience</h2>
+      <div className="container workexperience__container">
+        {/* Lucy */}
+        <div className="workexperience__content">
+          <article className="workexperience__details">
+            <div>
+              <h4>
+                LUCY (<em> 2 Years </em>)
+              </h4>
+              <h5>E-learning Front-End Web Developer</h5>
+
+              <small className="text-light">
+                I take care of the client-requested modifications to the
+                E-learning template, such as adding additional language or
+                creating a new template for a different subject.
+              </small>
+            </div>
+          </article>
+        </div>
+        {/* Lucy */}
+
+        {/* Flying */}
+        <div className="workexperience__content">
+          <article className="workexperience__details">
+            <div>
+              <h4>
+                Flying Dragon Network Philippines Inc. (<em> 1 Year </em>)
+              </h4>
+
+              <h5>Full Stack Web Developer</h5>
+
+              <small className="text-light">
+                I'm in charge of purchasing and deploying websites as well as
+                adding and maintaining all of the rummy websites, including the
+                PBN and the main ones. I also manage the server hosting in all
+                of the websites, the technical SEO, daily updates to the
+                sitemaps, indexing page, and the Graphics for the Rummy Website.
+              </small>
+            </div>
+          </article>
+        </div>
+        {/* Flying */}
+      </div>
+      <div className="container workexperience__container w-cont2">
+        {/* Solaire */}
+        <div className="workexperience__content">
+          <article className="workexperience__details">
+            <div>
+              <h4>
+                Flying Dragon Network Philippines Inc. (<em> 1 Year </em>)
+              </h4>
+
+              <h5>Full Stack Web Developer</h5>
+
+              <small className="text-light">
+                I'm in charge of purchasing and deploying websites as well as
+                adding and maintaining all of the rummy websites, including the
+                PBN and the main ones. I also manage the server hosting in all
+                of the websites, the technical SEO, daily updates to the
+                sitemaps, indexing page, and the Graphics for the Rummy Website.
+              </small>
+            </div>
+          </article>
+        </div>
+        {/* Solaire */}
+      </div>
+      <h5>
+        Note: I did not include my past work that not related to Web developing
+      </h5>
+    </section>
+  )
+}
+
+export default WorkExperience
